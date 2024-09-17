f18eb9400cec9dcf119c8c62c7b66c0555823e35
shafaat33 <shafaat@wanclouds.net>
2024-09-17 09:53:55 +0500

    added env file for securing keys and some enhancements

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import {IMAGE_CDN_URL} from "./utils/constants";
 
 const MovieCard = ({ posterPath }) => {
-  console.log(posterPath)
+  if (!posterPath) return null;
   return (
     <div className='w-36 md:w-48 p-2'>
       <img alt="movie cover" src={IMAGE_CDN_URL + posterPath}/>
--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -6,7 +6,7 @@ export const API_OPTIONS = {
   method: 'GET',
    headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMGQwNGMxMzFhZTQ0ZTI4NjU1M2ExN2RjNmM0OTY3YSIsInN1YiI6IjY1MDVkNDBjM2NkMTJjMDEwY2NmZGQ5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vEsb86bPAGWGYYapi1O-yfLq_LM8hZsRCav-J2gL6ZU',
+    Authorization: 'Bearer ' + process.env.REACT_APP_AUTHORIZATION_KEY,
   },
 };
 
@@ -16,6 +16,6 @@ export const IMAGE_CDN_URL = "https://image.tmdb.org/t/p/w500/";
 
 export const BG_IMAGE_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/bf6f5dab-8a85-48af-be22-de3a0cfd4ea7/PK-en-20230821-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-export const GPT_API_KEY = "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";
+export const GPT_API_KEY = process.env.REACT_APP_GPT_API_KEY;
 
 export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/bf6f5dab-8a85-48af-be22-de3a0cfd4ea7/PK-en-20230821-popsignuptwoweeks-perspective_alpha_website_large.jpg";
